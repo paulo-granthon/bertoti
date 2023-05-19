6c9914e09a08bad07586f0815efa42c341448a13
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-05-19 01:51:28 -0300

    Updated tests package location

---
+++ Engenharia de Software/pulgas/src/tests/DogTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.junit.Test;
+
+import pulgas.entities.Dog;
+import pulgas.entities.Flea;
+import pulgas.entities.Race;
+import pulgas.utils.RandGen;
+
+import static org.junit.Assert.*;
+
+public class DogTests {
+
+    @Test
+    public void testAddAndRemovePulga() {
+        Dog cachorro = new Dog("Test Dog", Race.randomRace(new RandGen()));
+        Flea pulga = new Flea("Test Pulga");
+        cachorro.addPulga(pulga);
+        assertTrue(cachorro.getPulgas().contains(pulga));
+
+        boolean result = cachorro.removePulga(pulga);
+        assertTrue(result);
+        assertFalse(cachorro.getPulgas().contains(pulga));
+    }
+
+}
---
+++ Engenharia de Software/pulgas/src/tests/YardTests.java
@@ -0,0 +1,37 @@
+package tests;
+import org.junit.Test;
+
+import pulgas.entities.Dog;
+import pulgas.entities.Flea;
+import pulgas.entities.Race;
+import pulgas.main.Yard;
+import pulgas.utils.RandGen;
+
+import static org.junit.Assert.*;
+
+public class YardTests {
+
+    @Test
+    public void testAddAndRemovePulga() {
+        Yard quintal = new Yard(new RandGen());
+        Flea pulga = new Flea("Test Pulga");
+        quintal.addFlea(pulga);
+        assertTrue(quintal.getFleas().contains(pulga));
+
+        boolean result = quintal.removePulga(pulga);
+        assertTrue(result);
+        assertFalse(quintal.getFleas().contains(pulga));
+    }
+
+    @Test
+    public void testAddAndRemoveCachorro() {
+        Yard quintal = new Yard(new RandGen());
+        Dog cachorro = new Dog("Test Dog", Race.randomRace(new RandGen()));
+        quintal.addCachorro(cachorro);
+        assertTrue(quintal.getDogs().contains(cachorro));
+
+        boolean result = quintal.removeCachorro(cachorro);
+        assertTrue(result);
+        assertFalse(quintal.getDogs().contains(cachorro));
+    }
+}

9485a19e93aed4b8b240a4c0b1f17b7868251fe2
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-06-11 17:03:20 -0300

    Revert Stock.java

---
+++ Engenharia de Software/pulgas/src/pulgas/submenus/Stock.java
@@ -0,0 +1,31 @@
+package pulgas.submenus;
+
+import java.util.LinkedList;
+
+import pulgas.products.Shampoo;
+import pulgas.utils.RandGen;
+
+public class Stock {
+
+    private LinkedList<Shampoo> shampoos;
+    private int money;
+
+    public Stock(RandGen randGen) {
+        shampoos = new LinkedList<>();
+        generateRandomStock(randGen);
+    }
+
+    private void generateRandomStock (RandGen randGen) {
+        while (randGen.happens(101, 25)) {
+            shampoos.add(Shampoo.random(randGen));
+        }
+        if (shampoos.size() < 2) generateRandomStock(randGen);
+    }
+
+    public LinkedList<Shampoo> getShampoos() { return shampoos; }
+    public int getMoney() { return money; }
+
+    public void addMoney (int amount) {
+        money += money;
+    }
+}

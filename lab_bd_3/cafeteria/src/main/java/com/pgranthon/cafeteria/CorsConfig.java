7775935557719b7cc91bf505526f361e16cb313b
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-09 16:44:52 -0300

    add: CorsConfig

---
+++ lab_bd_3/cafeteria/src/main/java/com/pgranthon/cafeteria/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.pgranthon.cafeteria;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}

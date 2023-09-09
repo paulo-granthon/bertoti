519c1cb6727dfdbd5bf1914107d5aa6e811cf50e
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-09 16:03:12 -0300

    fix (backend): `id` assign on `POST` | style (backend): fixed mixed-ident on `Main.java`

--- lab_bd_3/cafeteria/src/main/java/com/pgranthon/cafeteria/Main.java
+++ lab_bd_3/cafeteria/src/main/java/com/pgranthon/cafeteria/Main.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -14,9 +16,9 @@ import java.util.UUID;
 @SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 
 }
 
@@ -24,83 +26,81 @@ public class Main {
 @RequestMapping("/coffees")
 @CrossOrigin(origins = "*")
 class RestApiDemoController {
-	private List<Coffee> coffees = new ArrayList<>();
+    private List<Coffee> coffees = new ArrayList<>();
 
-	public RestApiDemoController() {
-		coffees.addAll(List.of(
+    public RestApiDemoController() {
+        coffees.addAll(List.of(
             new Coffee("Café Cereza"),
             new Coffee("Café Ganador"),
             new Coffee("Café Lareño"),
             new Coffee("Café Três Pontas")
-		));
-	}
+        ));
+    }
 
-	@GetMapping
-	Iterable<Coffee> getCoffees() {
-		return coffees;
-	}
+    @GetMapping
+    Iterable<Coffee> getCoffees() {
+        return coffees;
+    }
 
-	@GetMapping("/{id}")
-	Optional<Coffee> getCoffeeById(@PathVariable String id) {
-		for (Coffee c: coffees) {
-			if (c.getId().equals(id)) {
-				return Optional.of(c);
-			}
-		}
+    @GetMapping("/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id) {
+        for (Coffee c: coffees) {
+            if (c.getId().equals(id)) {
+                return Optional.of(c);
+            }
+        }
 
-		return Optional.empty();
-	}
+        return Optional.empty();
+    }
 
-	@PostMapping
-	Coffee postCoffee(@RequestBody Coffee coffee) {
-		coffees.add(coffee);
-		return coffee;
-	}
+    @PostMapping
+    @CrossOrigin(origins = "*")
+    Coffee postCoffee(@RequestBody Coffee coffee) {
+        coffees.add(coffee);
+        return coffee;
+    }
 
-	@PutMapping("/{id}")
-	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-		int coffeeIndex = -1;
+    @PutMapping("/{id}")
+    ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+        int coffeeIndex = -1;
 
-		for (Coffee c: coffees) {
-			if (c.getId().equals(id)) {
-				coffeeIndex = coffees.indexOf(c);
-				coffees.set(coffeeIndex, coffee);
-			}
-		}
+        for (Coffee c: coffees) {
+            if (c.getId().equals(id)) {
+                coffeeIndex = coffees.indexOf(c);
+                coffees.set(coffeeIndex, coffee);
+            }
+        }
 
-		return (coffeeIndex == -1) ?
+        return (coffeeIndex == -1) ?
             new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
             new ResponseEntity<>(coffee, HttpStatus.OK);
-	}
+    }
 
-	@DeleteMapping("/{id}")
-	void deleteCoffee(@PathVariable String id) {
-		coffees.removeIf(c -> c.getId().equals(id));
-	}
+    @DeleteMapping("/{id}")
+    void deleteCoffee(@PathVariable String id) {
+        coffees.removeIf(c -> c.getId().equals(id));
+    }
 }
 
 class Coffee {
-	private final String id;
-	private String name;
+    private final String id;
+    private String name;
 
-	public Coffee(String id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+    public Coffee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public Coffee(String name) {
-		this(UUID.randomUUID().toString(), name);
-	}
+    @JsonCreator
+    public Coffee(String name) {
+        this(
+            UUID.randomUUID().toString(),
+            name
+        );
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() { return id; }
+    public String getName() { return name; }
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName (String name) { this.name = name; }
 }

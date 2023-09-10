ac8c1cd5635b1b791b4656c199257cdbfa33b87e
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-10 11:34:38 -0300

    add (ApiService): `putCoffee` function

--- lab_bd_3/cafeteria_front/src/services/ApiService.js
+++ lab_bd_3/cafeteria_front/src/services/ApiService.js
@@ -27,5 +27,27 @@ export async function postCoffee(coffee) {
     } catch (error) {
         console.error('API -- POST Error:', error);
         return error;
-        }
+    }
+}
+
+export async function putCoffee(coffee) {
+    return await fetch(`${API_URL}/coffees/${coffee.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: coffee.name })
+    })
+    .then(response => response.json())
+    .then(data => console.log('Coffee updated:', data))
+    .catch(error => console.error('API -- PUT Error:', error));
+}
+
+export async function deleteCoffee(coffee) {
+    return await fetch(`${API_URL}/coffees/${coffee.id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
 }

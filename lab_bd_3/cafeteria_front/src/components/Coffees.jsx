ac88f05e014cbd04c0770cec40b18d455c0c0788
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-09-10 09:43:13 -0300

    add: DELETE button to `Coffees` component

--- lab_bd_3/cafeteria_front/src/components/Coffees.jsx
+++ lab_bd_3/cafeteria_front/src/components/Coffees.jsx
@@ -1,18 +1,33 @@
 
-const Coffee = ({ coffee }) => {
+const Coffee = ({ coffee, onChange }) => {
+    const removeCoffee = () => {
+        const coffeeId = coffee.id;
+        console.log('coffeeId: ', coffee);
+        fetch(`http://localhost:8080/coffees/${coffeeId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(() => onChange())
+        .catch((error) => {
+            console.error('Error deleting coffee:', error);
+        });
+    };
     return (
         <div className="coffee-container">
             <p className="coffee-name">{coffee.name}</p>
+            <p className="coffee-remove" onClick={removeCoffee}>X</p>
         </div>
     )
 }
 
-export default function Coffees ({ coffees }) {
+export default function Coffees ({ coffees, onChange }) {
     return (
         <div className="coffees-container">
             {coffees != null ? (
                 <ul className="coffees-ul">
-                    {coffees.map(coffee => <Coffee key={coffee.id} coffee={coffee} />)}
+                    {coffees.map(coffee => <Coffee key={coffee.id} coffee={coffee} onChange={onChange} />)}
                 </ul>
             ) : (
                 <p className="coffees-ul-loading">Loading coffees...</p>

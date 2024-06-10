9e84d7541c83bf9cd3aba499f756fe3c280c973d
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-06-10 01:10:42 -0300

    feat: add `Singleton` pattern file in `patterns`

---
+++ patterns/singleton/Singleton.java
@@ -0,0 +1,25 @@
+package singleton;
+
+public class Singleton {
+  public class Logger {
+    private static Logger instance;
+
+    private Logger() {}
+
+    public static Logger getInstance() {
+      if (instance == null) {
+        instance = new Logger();
+      }
+      return instance;
+    }
+
+    public void log(String message) {
+      System.out.println(message);
+    }
+  }
+
+  public static void main(String[] args) {
+    Logger logger = Logger.getInstance();
+    logger.log("Hello, world!");
+  }
+}
